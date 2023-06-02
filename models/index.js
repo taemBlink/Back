93d21371d37564861e6ef970cc145390ca3e8924
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-06-03 00:25:06 +0900

    Merge branch 'main' of https://github.com/taemBlink/Back
    # Conflicts:
    #	.github/workflows/main.yml

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const { Sequelize } = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const { Sequelize } = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 // const config = require(__dirname + '/../config/config.json')[env];
-const config = require('../config/config.js')[env];
+const config = require("../config/config.js")[env];
 const db = {};
 
 let sequelize;
@@ -22,22 +22,24 @@ if (config.use_env_variable) {
   });
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
--- routes/jobs.js
+++ routes/jobs.js
@@ -14,7 +14,7 @@ const fileStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     // 저장되는 이름 지정
-    cb(null, file.originalname);
+    cb(null, File.name);
   },
 });
 
@@ -40,7 +40,7 @@ const fileFilter = (req, file, cb) => {
 };
 // image_file 저장
 const upload = multer({ storage: fileStorage, fileFilter: fileFilter }).single(
-  "image"
+  "File"
 );
 
 // 0. 이미지 파일 업로드 API
