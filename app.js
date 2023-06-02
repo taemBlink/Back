76629be956d7a2cfe9b9b9212534b49e6532ace1
chang <https://github.com/BYUNCHANGILL>
2023-06-02 22:52:36 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const session = require("express-session");
 // const cors = require('cors');
 const app = express();
 require("dotenv").config();
-require('./config/config.js')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
